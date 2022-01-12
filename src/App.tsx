e9a8c6caf4a48a0140c9b10b61920ff133a7e98d
chansaem <ased1233@naver.com>
2022-01-12 23:28:16 +0900

    Variants

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,32 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
+const boxVars = {
+  Hover: { scale: 1.5, rotateZ: 90 },
+  Tap: { scale: 1, borderRadius: "100px" },
+  Drag: { backgroundColor: "rgb(100,100,100)", transition: { duration: 5 } },
+};
 
 function App() {
   return (
     <Wrapper>
-      <Box />
-      <motion.div></motion.div>
+      <Box
+        drag
+        variants={boxVars}
+        whileDrag="Drag"
+        whileHover="Hover"
+        whileTap="Tap"
+      ></Box>
     </Wrapper>
   );
 }
