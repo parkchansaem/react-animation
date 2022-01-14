dfaaeee27d342c61ec5fdbaa5178926b458feb43
chansaem <ased1233@naver.com>
2022-01-14 15:07:52 +0900

    slider bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import {
+  AnimatePresence,
   motion,
   useMotionValue,
   useTransform,
   useViewportScroll,
 } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled(motion.div)`
@@ -13,6 +14,7 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
 /* const BiggerBox = styled.div`
   width: 600px;
@@ -33,6 +35,11 @@ const Box = styled(motion.div)`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 100px;
 `;
 
 const boxVars = {
@@ -40,26 +47,50 @@ const boxVars = {
   Tap: { scale: 1, borderRadius: "100px" },
   Drag: { backgroundColor: "rgb(100,100,100)", transition: { duration: 5 } },
 };
-const Svg = styled(motion.svg)`
-  width: 200px;
-  height: 200px;
-  color: white;
-`;
+
+const BoxV = {
+  initial: (back: boolean) => ({
+    opacity: 0,
+    scale: 0,
+    x: back ? -500 : 500,
+  }),
+  visible: { opacity: 1, scale: 1, x: 0, transition: { duration: 1 } },
+  exit: (back: boolean) => ({
+    opacity: 0,
+    scale: 0,
+    x: back ? 500 : -500,
+    rotateZ: 360,
+    transition: { duration: 1 },
+  }),
+};
+
 function App() {
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const next = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prev = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
   return (
     <Wrapper>
-      <Svg
-        focusable="false"
-        xmlns="http://www.w3.org/2000/Svg"
-        viewBox="0 0 448 512"
-      >
-        <path
-          stroke="white"
-          strokeWidth={2}
-          fill="transparent"
-          d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
-        ></path>
-      </Svg>
+      <button onClick={next}>next</button>
+      <button onClick={prev}>prev</button>
+      <AnimatePresence exitBeforeEnter custom={back}>
+        <Box
+          custom={back}
+          variants={BoxV}
+          initial="initial"
+          animate="visible"
+          exit="exit"
+          key={visible}
+        >
+          {visible}
+        </Box>
+      </AnimatePresence>
     </Wrapper>
   );
 }
